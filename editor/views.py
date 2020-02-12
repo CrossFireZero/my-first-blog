ec7a1d13a2eea9ccd807a3c8b349cf03f7780bf1
Max <robotland.ru@gmail.com>
2020-02-12 17:50:23 +0300

    now taking code from form to .py file

--- editor/views.py
+++ editor/views.py
@@ -2,12 +2,26 @@ from .forms import SnippetForm
 from .models import Snippet
 from django.shortcuts import render, redirect
 
+# для запуска shell команд
+import subprocess
+
 
 def simple(request):
     if request.method == 'POST':
         form = SnippetForm(request.POST)
         if form.is_valid():
             form.save()
+
+            # тут забираю код с формы и оформляю его в виде .py файла
+            print("RAW POST DATA: {}".format(request.POST['text']))
+            lines = request.POST['text'].split('\n')
+            with open('temp.py', 'w', encoding='utf-8') as f:
+                for line in lines:
+                    f.write(line)
+                
+            subprocess.run(["python", "temp.py"])
+            
+
             return redirect('/editor')
     else:
         form = SnippetForm()
